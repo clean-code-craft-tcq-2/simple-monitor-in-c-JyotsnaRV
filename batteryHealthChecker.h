88dfbb4ae0c9a7263c781192c73d2f519e3500b0
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-02-09 00:51:25 +0530

    Create batteryHealthChecker.h

---
+++ batteryHealthChecker.h
@@ -0,0 +1,7 @@
+#ifndef BATTERY_HEALTH_CHECKER_H
+#define BATTERY_HEALTH_CHECKER_H
+
+/* Declarations */
+int batteryIsOk(float temperature, float soc, float chargeRate);
+
+#endif /* BATTERY_HEALTH_CHECKER_H */
