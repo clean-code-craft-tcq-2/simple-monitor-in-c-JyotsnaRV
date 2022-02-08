71586d4bd61cb35e813bb5b6594a780e43eb51c4
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-02-09 01:30:10 +0530

    Update macros for fixed limits

--- BmsCheckers.h
+++ BmsCheckers.h
@@ -4,6 +4,14 @@
 /* Necessary includes */
 #include <stdio.h>
 
+/* Macros */
+#define TEMPERATURE_MIN 0
+#define TEMPERATURE_MAX 45
+#define SOC_MIN 20
+#define SOC_MAX 80
+#define CHARGE_RATE_MIN 0.0
+#define CHARGE_RATE_MAX 0.8
+
 /* Declarations */
 float temperatureCheck(float temperature);
 float statusOfCharge(float soc);
