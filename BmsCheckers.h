c9b0dc7fd3192974b64c5d9a2ae237b68f1e3faf
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-02-09 01:12:54 +0530

    Add stdio inclusion to solve warning

--- BmsCheckers.h
+++ BmsCheckers.h
@@ -1,6 +1,9 @@
 #ifndef BMS_CHECKERS_H
 #define BMS_CHECKERS_H
 
+/* Necessary includes */
+#include <stdio.h>
+
 /* Declarations */
 float temperatureCheck(float temperature);
 float statusOfCharge(float soc);
