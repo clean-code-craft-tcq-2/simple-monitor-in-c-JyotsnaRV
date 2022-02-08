dc12dcc7e176988cc7dbfe99c06e1ac25c8af3d9
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-02-09 01:08:34 +0530

    Create BmsCheckers.h

---
+++ BmsCheckers.h
@@ -0,0 +1,9 @@
+#ifndef BMS_CHECKERS_H
+#define BMS_CHECKERS_H
+
+/* Declarations */
+float temperatureCheck(float temperature);
+float statusOfCharge(float soc);
+float rateOfCharge(float chargeRate);
+
+#endif /* BMS_CHECKERS_H */
