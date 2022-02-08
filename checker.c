9e1bb719acee213b09efee42baca0eb79d8a440a
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-02-09 00:52:48 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
+#include "batteryHealthChecker.h"
 
+/* Drive functions to test the battery health based on the given parameters */
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
