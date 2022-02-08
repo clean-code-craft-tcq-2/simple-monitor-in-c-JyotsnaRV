bbac06814c26e9de493e0170c6f70c57b74350cd
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-02-09 01:43:11 +0530

    Limit decimal places for float

--- BmsCheckers.c
+++ BmsCheckers.c
@@ -4,12 +4,12 @@ float temperatureCheck(float temperature)
 {
   if(temperature < TEMPERATURE_MIN || temperature > TEMPERATURE_MAX)
   {
-    printf("Temperature %f out of range!\n", temperature);
+    printf("Temperature %.2f out of range!\n", temperature);
     return 0;
   }
   else
   {
-    printf("Temperature %f OK\n", temperature);
+    printf("Temperature %.2f OK\n", temperature);
     return 1;
   }
 }
@@ -18,12 +18,12 @@ float statusOfCharge(float soc)
 {
   if(soc < SOC_MIN || soc > SOC_MAX)
   {
-    printf("State of Charge %f out of range!\n", soc);
+    printf("State of Charge %.2f out of range!\n", soc);
     return 0;
   }
   else
   {
-    printf("State of Charge %f OK\n", soc);
+    printf("State of Charge %.2f OK\n", soc);
     return 1;
   }
 }
@@ -32,12 +32,12 @@ float rateOfCharge(float chargeRate)
 {
   if(chargeRate <= CHARGE_RATE_MIN || chargeRate > CHARGE_RATE_MAX)
   {
-    printf("Charge Rate %f out of range!\n", chargeRate);
+    printf("Charge Rate %.2f out of range!\n", chargeRate);
     return 0;
   }
   else
   {
-    printf("Charge Rate %f OK\n", chargeRate);
+    printf("Charge Rate %.2f OK\n", chargeRate);
     return 1;
   }
 }
