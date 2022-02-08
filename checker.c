2b99f8e51afb3552652988e2b0c07839c65c9fdb
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-02-09 01:59:54 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -4,8 +4,10 @@
 
 /* Drive functions to test the battery health based on the given parameters */
 int main() {
+  /* Positive Testcase - common */
   assert(batteryIsOk(25, 70, 0.7));
-  assert(!batteryIsOk(50, 85, 0));
-  assert(!batteryIsOk(45.5, 70, 0.6));
-  assert(!batteryIsOk(30, 75, 0.9));
+  /* Negative Testcases */
+  assert(!batteryIsOk(-0.5, 75, 0.9));
+  assert(!batteryIsOk(45.5, 95, 0.6));
+  assert(!batteryIsOk(25, 85, 0));
 }
