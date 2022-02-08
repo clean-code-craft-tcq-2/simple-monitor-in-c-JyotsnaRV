e2434c8882e800a6a7c64b2e31dbe668e9dde975
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-02-09 01:53:05 +0530

    Split error handling for min and max limits

--- BmsCheckers.c
+++ BmsCheckers.c
@@ -21,9 +21,14 @@ float temperatureCheck(float temperature)
 
 float statusOfCharge(float soc)
 {
-  if(soc < SOC_MIN || soc > SOC_MAX)
+  if(soc < SOC_MIN)
   {
-    printf("State of Charge %.2f out of range!\n", soc);
+    printf("State of Charge %.2f crossed minimum threshold value! Increase the SoC of the system!\n", soc);
+    return 0;
+  }
+  else if(soc > SOC_MAX)
+  {
+    printf("State of Charge %.2f crossed maximum threshold value! Reduce the SoC of the system!\n", soc);
     return 0;
   }
   else
@@ -35,9 +40,14 @@ float statusOfCharge(float soc)
 
 float rateOfCharge(float chargeRate)
 {
-  if(chargeRate <= CHARGE_RATE_MIN || chargeRate > CHARGE_RATE_MAX)
+  if(chargeRate <= CHARGE_RATE_MIN)
   {
-    printf("Charge Rate %.2f out of range!\n", chargeRate);
+    printf("Charge Rate %.2f is too low! Increase the Rate of Charge of the system.\n", chargeRate);
+    return 0;
+  }
+  else if(chargeRate > CHARGE_RATE_MAX)
+  {
+    printf("Charge Rate %.2f is too high! Decrease the Rate of Charge of the system.\n", chargeRate);
     return 0;
   }
   else
