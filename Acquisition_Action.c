5ce2259fd7ef810d674c02f3d7bac491ce67af75
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-03-09 09:32:46 +0530

    Create Acquisition_Action.c

---
+++ Acquisition_Action.c
@@ -0,0 +1,19 @@
+#include "BmsCheckers.h"
+#include "Data_Acquisition_Report.h"
+
+ackRange_st dataRange;
+/*
+ **********************************************************************************************************************
+ * Service name         : dataAccumalator
+ * Syntax               : void dataAcquisition(toleranceRange_st range)
+ * param[in]            : range
+ * return               : None
+ * Description          : Program to check and accumalte the required BMS data 
+ **********************************************************************************************************************
+ */
+
+void dataAcquisition(toleranceRange_st range)
+{
+  warningsGenerator(range);
+  (void)controllerAction();
+}
