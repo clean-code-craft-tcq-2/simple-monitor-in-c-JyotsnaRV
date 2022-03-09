7c90ab6149bd84296125f40d0b1470884fce5c0b
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-03-09 08:00:04 +0530

    Update BmsCheckers.h

--- BmsCheckers.h
+++ BmsCheckers.h
@@ -5,15 +5,45 @@
 #include <stdio.h>
 
 /* Macros */
+#define NUM_OF_DOMAINS 3
 #define TEMPERATURE_MIN 0
 #define TEMPERATURE_MAX 45
-#define SOC_MIN 20
-#define SOC_MAX 80
+#define TEMPERATURE_RESIDENTIAL_MAX 80
+#define TEMPERATURE_CONSUMER_MAX 200
+#define TEMPERATURE_EV_MAX 150
+#define SOC_MIN (float)20
+#define SOC_MAX (float)80
 #define CHARGE_RATE_MIN 0.0
 #define CHARGE_RATE_MAX 0.8
 
+#define BINARY_MIN 0
+#define BINARY_MAX 1
+
+#define WARNING_MSG_CHAR 40
+
+/* Macros to perform extension-3: temperature unit conversion */
+#define CALCTOLERANCE(x) (float)(x*(0.05))
+#define CEL 0 /* celcius */
+#define FAHR 1 /* Fahrenheit */
+#define CELTOFAHR(Y) (float)((Y*(9/5))+32)
+
+/* Enumerations */
+enum MajorDomain {RESIDENTIAL, CONSUMER, EV};
+typedef enum {TEMPERATURE, SOC, ROC} BmsParameter;
+
+/* Structures */
+typedef struct
+{
+	float inputParameter;
+	float minTolerance;
+	float maxTolerance;
+	BmsParameter paramType;
+	int dataRange;
+	int tempUnitConv;
+}toleranceRange_st;
+
 /* Declarations */
-float IsTemperatureOk(float temperature);
+float IsTemperatureOk(float temperature,enum MajorDomain domainType, int temperatureSIUnit);
 float statusOfCharge(float soc);
 float rateOfCharge(float chargeRate);
 
