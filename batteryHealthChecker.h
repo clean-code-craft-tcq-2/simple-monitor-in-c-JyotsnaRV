a8d5da15c8349f72d9ce2cfe9e2fdaa0d5f49c46
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-03-09 07:58:06 +0530

    Update batteryHealthChecker.h

--- batteryHealthChecker.h
+++ batteryHealthChecker.h
@@ -2,6 +2,6 @@
 #define BATTERY_HEALTH_CHECKER_H
 
 /* Declarations */
-int batteryIsOk(float temperature, float soc, float chargeRate);
+int batteryIsOk(float temperature, float soc, float chargeRate, enum MajorDomain domain, int temperatureSIUnit);
 
 #endif /* BATTERY_HEALTH_CHECKER_H */
