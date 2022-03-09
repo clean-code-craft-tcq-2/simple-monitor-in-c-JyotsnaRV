60401b51f1704ea788b8aac59082a0aa3a409f74
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-03-09 09:10:00 +0530

    Create RateOfCharge.c

---
+++ RateOfCharge.c
@@ -0,0 +1,35 @@
+#include "BmsCheckers.h"
+#include "Data_Acquisition_Report.h"
+
+extern acqRange_st dataRange;
+/*
+ **********************************************************************************************************************
+ * Service name         : rateOfCharge
+ * Syntax               : int rateOfCharge(float chargeRate) 
+ * param[in]            : chargeRate
+ * return               : 0 - out of range, 1 - in range
+ * Description          : Program to verify if Charge Rate is out of range
+ **********************************************************************************************************************
+ */
+int rateOfCharge(float chargeRate)
+{
+  int retStatus = 1;
+  toleranceRange_st rocRange;
+  if(chargeRate > RATEOFCHARGE)
+  {
+    printf("Charge Rate(%f) is more than the expected range (%f)!\n",chargeRate,RATEOFCHARGE);
+    retStatus = 0;
+    rocRange.minTolerance = RATEOFCHARGE + (float)0.1;
+    rocRange.maxTolerance = 2.0;
+    rocRange.paramType = ROC;
+    rocRange.inputParam = chargeRate;
+    rocRange.tempUnitConv = 0;
+    rocRange = checkTolerance(rocRange);
+    dataRange.rocInputParam = rocRange.inputParam;
+    dataRange.rocMinBreach = rocRange.minTolerance;
+    dataRange.rocMaxBreach = rocRange.maxTolerance;
+    dataAcquisition(rocRange);
+  }
+  dataRange.retstatus = retStatus;
+  return retStatus;
+}
