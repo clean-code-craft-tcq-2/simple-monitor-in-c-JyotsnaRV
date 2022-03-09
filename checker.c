8bcd95572a973f3861cf79236e1760633b85d733
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-03-09 09:39:16 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -13,4 +13,16 @@ int main() {
   assert(!batteryIsOk(25, 15, 0));
   assert(!batteryIsOk(25, 65, 0));
   assert(!batteryIsOk(25, 85, 0.9));
+  
+  /* Positive test case - BMS Passed (Temperature, Soc and Charge rate is within expected range) */
+  assert(!batteryHealth(80, 85, 0,RESIDENTIAL,FAHR));
+  assert(!batteryHealth(190, 85, 0,CONSUMER,CEL));
+  assert(!batteryHealth(140, 85, 0,EV,FAHR));
+  
+  /* Function verification */
+  batteryHealth(25, 70, 0,RESIDENTIAL,CEL);   /* Temperature in range */
+  batteryHealth(55, 85, 0,RESIDENTIAL,FAHR);  /* soc out of range */
+  batteryHealth(55, 70, 0.9,CONSUMER,CEL);    /* charge rate not in range */
+  batteryHealth(45, 70, 0,EV,FAHR);           /* everything in range */
+  batteryHealth(45, 70, 0,(int)5,CEL);        /* domain out of range */
 }
