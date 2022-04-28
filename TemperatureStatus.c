2c55c3a74da4767ba91c559f78cabe26a399586b
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-04-28 16:32:30 +0530

    Update TemperatureStatus.c

--- TemperatureStatus.c
+++ TemperatureStatus.c
@@ -17,11 +17,11 @@ int temperatureStatus(float temperature, float compareTemp, int tempSIUnit)
 	int retStatus = 1;
 	toleranceRange_st tempRange;
 	/* Check if the temperature is out of range */
-	if((temperature>(float)TEMP_MIN) && (temperature > cmprTemperature))
+	if((temperature>(float)TEMPERATURE_MIN) && (temperature > cmprTemperature))
 	{
 		retStatus = 0;
-                printf("temperature %f has breached the expected range(%d - %f)celcius!\n",temperature,TEMP_MIN,cmprTemperature);
-		tempRange.minTolerance = (float)TEMP_MIN;
+                printf("temperature %f has breached the expected range(%d - %f)celcius!\n",temperature,TEMPERATURE_MIN,cmprTemperature);
+		tempRange.minTolerance = (float)TEMPERATURE_MIN;
 		tempRange.maxTolerance = cmprTemperature;
 		tempRange.paramType = TEMPERATURE;
 		tempRange.inputParam = temperature;
