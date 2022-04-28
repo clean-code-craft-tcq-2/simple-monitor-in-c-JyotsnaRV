57264533be2441243990e5cb44c211681ac0af7e
JyotsnaRV <91152645+JyotsnaRV@users.noreply.github.com>
2022-04-28 17:12:20 +0530

    Delete batteryHealthChecker.h

--- batteryHealthChecker.h
+++
@@ -1,7 +0,0 @@
-#ifndef BATTERY_HEALTH_CHECKER_H
-#define BATTERY_HEALTH_CHECKER_H
-
-/* Declarations */
-int batteryIsOk(float temperature, float soc, float chargeRate, enum MajorDomain domain, int temperatureSIUnit);
-
-#endif /* BATTERY_HEALTH_CHECKER_H */
